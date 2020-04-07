6c78e3c8aaeb64ce9a96a5326ac8d8266ccf694a
rosterry58 <terrycajorosmery@gmail.com>
2020-04-06 20:04:33 -0500

    segundo cambio-06

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import {
   orderData,
   searchChampions,
   orderFilterTags,
+  dataOrdenado,
 } from './data.js';
 
 const btnMostrar = document.getElementById('btnMostrar');
@@ -35,7 +36,6 @@ const championList = data.data;
 const contentList = document.querySelector('#contentList');
 const contentStats = document.querySelector('#contentStats');
 const championArray = Object.values(championList);
-
 const listOnScreen = (dataLol) => {
   let result = '';
   dataLol.forEach((champion) => {
@@ -112,4 +112,147 @@ enterText.addEventListener('keyup', () => {
       `;
   }
 });
-listOnScreen(championArray);
+
+
+
+
+const contentListRol = document.querySelector('#contentListRol');
+const listOnScreenRol = (dataLol) => {
+  let result = '';
+  dataLol.forEach((champion) => {
+    const showChampions = `
+    <section>
+      <img src=${champion.splash} class="imgSplash">
+      <p class="name">${champion.name}</p><br><br>
+       <p class="tags">Tags:</span> ${champion.tags}</p><br>
+     </section> 
+      `;
+    result += showChampions;
+  });
+  contentListRol.innerHTML = result;
+};
+listOnScreenRol(championArray);
+/* ORDENAR CAMPEONES ASCENDENTE - DESCENDENTE */
+const orderRoles = document.querySelector('#orderRoles');
+orderRoles.addEventListener('change', () => {
+  const orderSelect = orderRoles.value;
+  listOnScreenRol(orderData(championArray, 'name', orderSelect));
+});
+/* BUSCAR POR LETRA CADA CAMPEON */
+const inputBuscarRoles = document.querySelector('#inputBuscarRoles');
+inputBuscarRoles.addEventListener('keyup', () => {
+  const text = inputBuscarRoles.value.toLowerCase();
+  listOnScreenRol(searchChampions(championArray, 'name', text));
+  if (contentListRol.innerHTML === '') {
+    contentListRol.innerHTML += `
+      <p>ERROR!!!</p>
+      `;
+  }
+});
+
+/* ORDENAR POR ROL ASESINO */
+const video = document.getElementById('video');
+const mostrarAsesinos = document.getElementById('rolAssassin');
+mostrarAsesinos.addEventListener('click', () => {
+  video.classList.add('classNone');
+  thirdScreen.classList.remove('classNone');
+  fourthScreen.classList.remove('classNone');
+});
+const ordenarAsesinos = document.querySelector('#rolAssassin');
+ordenarAsesinos.addEventListener('click', () => {
+  const ordenar = ordenarAsesinos.value;
+  listOnScreenRol(orderFilterTags(championArray, 'tags', ordenar));
+});
+/* ORDENAR POR ROL LUCHADOR */
+const mostrarTiradores = document.getElementById('rolFighter');
+mostrarTiradores.addEventListener('click', () => {
+  thirdScreen.classList.remove('classNone');
+  fourthScreen.classList.remove('classNone');
+});
+const ordenarLuchador = document.querySelector('#rolFighter');
+ordenarLuchador.addEventListener('click', () => {
+  const ordenar = ordenarLuchador.value;
+  listOnScreenRol(orderFilterTags(championArray, 'tags', ordenar));
+});
+/* ORDENAR POR ROL MAGOS */
+const mostrarMagos = document.getElementById('rolMage');
+mostrarMagos.addEventListener('click', () => {
+  thirdScreen.classList.remove('classNone');
+  fourthScreen.classList.remove('classNone');
+});
+const ordenarMago = document.querySelector('#rolMage');
+ordenarMago.addEventListener('click', () => {
+  const ordenar = ordenarMago.value;
+  listOnScreenRol(orderFilterTags(championArray, 'tags', ordenar));
+});
+/* ORDENAR POR ROL TIRADORES */
+const mostrarTirador = document.getElementById('rolMarksman');
+mostrarTirador.addEventListener('click', () => {
+  thirdScreen.classList.remove('classNone');
+  fourthScreen.classList.remove('classNone');
+});
+const ordenarTirador = document.querySelector('#rolMarksman');
+ordenarTirador.addEventListener('click', () => {
+  const ordenar = ordenarTirador.value;
+  listOnScreenRol(orderFilterTags(championArray, 'tags', ordenar));
+});
+/* ORDENAR POR ROL SOPORTE */
+const mostrarSoporte = document.getElementById('rolSupport');
+mostrarSoporte.addEventListener('click', () => {
+  thirdScreen.classList.remove('classNone');
+  fourthScreen.classList.remove('classNone');
+});
+const ordenarSoporte = document.querySelector('#rolSupport');
+ordenarSoporte.addEventListener('click', () => {
+  const ordenar = ordenarSoporte.value;
+  listOnScreenRol(orderFilterTags(championArray, 'tags', ordenar));
+});
+/* ROL MAGOS */
+const mostrarTank = document.getElementById('rolTank');
+mostrarTank.addEventListener('click', () => {
+  thirdScreen.classList.remove('classNone');
+  fourthScreen.classList.remove('classNone');
+});
+const ordenarTank = document.querySelector('#rolTank');
+ordenarTank.addEventListener('click', () => {
+  const ordenar = ordenarTank.value;
+  listOnScreenRol(orderFilterTags(championArray, 'tags', ordenar));
+});
+/* PROMEDIO DE ESTADÍSTICAS DEFENSIVAS */
+const obStats = (arr) => {
+  const newArray = arr.map(ele => ({
+    splash: ele.splash,
+    name: ele.name,
+    sumaHp: ele.stats.hp + ele.stats.armor + ele.stats.armorperlevel
+      + ele.stats.spellblock + ele.stats.spellblockperlevel,
+    promedio: ((ele.stats.hp + ele.stats.armor + ele.stats.armorperlevel
+        + ele.stats.spellblock + ele.stats.spellblockperlevel) / 5).toFixed(2),
+  }));
+  return newArray;
+};
+obStats(championArray);
+
+const contentTop = document.querySelector('#contentTop');
+const fTop = (dataLol) => {
+  let result = '';
+  dataLol.forEach((champion) => {
+    const showChampions = `
+    <section>
+    <img src=${champion.splash} class="imgSplash">
+    <p class="name">${champion.name}</p>
+    <p>${champion.promedio}</p>
+    </section>    
+      `;
+    result += showChampions;
+  });
+  contentTop.innerHTML = result;
+};
+
+const topTeam = document.getElementById('topTeam');
+const fifthScreen = document.getElementById('fifthScreen');
+topTeam.addEventListener('click', () => {
+  secondScreen.classList.toggle('classNone');
+  firstScreen.classList.add('classNone');
+  fifthScreen.classList.remove('classNone');
+  fTop(dataOrdenado(obStats(championArray)));
+});
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,74 +1,209 @@
-import { orderData, searchChampions } from '../src/data.js';
+import {
+  orderData,
+  searchChampions,
+  orderFilterTags,
+  dataOrdenado,
+} from '../src/data.js';
 
-const champAscend = [
+const champA = [
   {
     id: 'Morgana',
     key: 25,
     name: 'Morgana',
     title: 'Fallen Angel',
+    tags: ['Support'],
   },
   {
     id: 'Rammus',
     key: 33,
     name: 'Rammus',
     title: 'the Armordillo',
+    tags: ['Tank'],
   },
   {
     id: 'Shaco',
     key: 35,
     name: 'Shaco',
     title: 'the Demon Jester',
+    tags: ['Assassin'],
   },
   {
     id: 'Shen',
     key: 98,
     name: 'Shen',
     title: 'the Eye of Twilight',
+    tags: ['Tank'],
   },
 ];
-const champDescend = [
+const searchA = [
   {
-    id: 'Shen',
-    key: 98,
-    name: 'Shen',
-    title: 'the Eye of Twilight',
+    id: 'Rammus',
+    key: 33,
+    name: 'Rammus',
+    title: 'the Armordillo',
+    tags: ['Tank'],
   },
   {
     id: 'Shaco',
     key: 35,
     name: 'Shaco',
     title: 'the Demon Jester',
+    tags: ['Assassin'],
+  },
+  {
+    id: 'Shen',
+    key: 98,
+    name: 'Shen',
+    title: 'the Eye of Twilight',
+    tags: ['Tank'],
+  },
+];
+const champD = [
+  {
+    id: 'Shen',
+    key: 98,
+    name: 'Shen',
+    title: 'the Eye of Twilight',
+    tags: ['Tank', 'Melee'],
+  },
+  {
+    id: 'Shaco',
+    key: 35,
+    name: 'Shaco',
+    title: 'the Demon Jester',
+    tags: ['Assassin'],
   },
   {
     id: 'Rammus',
     key: 33,
     name: 'Rammus',
     title: 'the Armordillo',
+    tags: ['Tank', 'Fighter'],
   },
   {
     id: 'Morgana',
     key: 25,
     name: 'Morgana',
     title: 'Fallen Angel',
+    tags: ['Mage', 'Support'],
   },
 ];
-describe('orderData', () => {
+const rolTank = [
+  {
+    id: 'Rammus',
+    key: 33,
+    name: 'Rammus',
+    title: 'the Armordillo',
+    tags: ['Tank'],
+  },
+  {
+    id: 'Shen',
+    key: 98,
+    name: 'Shen',
+    title: 'the Eye of Twilight',
+    tags: ['Tank'],
+  },
+];
+const rolAsse = [
+  {
+    id: 'Shaco',
+    key: 35,
+    name: 'Shaco',
+    title: 'the Demon Jester',
+    tags: ['Assassin'],
+  },
+];
+const rolSupport = [
+  {
+    id: 'Morgana',
+    key: 25,
+    name: 'Morgana',
+    title: 'Fallen Angel',
+    tags: ['Support'],
+  },
+];
+const calculo = [
+  {
+    name: 'Alistar',
+    promedio: 134.92,
+  },
+  {
+    name: 'Tryndamere',
+    promedio: 137.24,
+  },
+  {
+    name: 'Trundle',
+    promedio: 135.97,
+  },
+  {
+    name: 'Garen',
+    promedio: 136.03,
+  },
+  {
+    name: 'Zac',
+    promedio: 135.07,
+  },
+];
+const ordenado = [
+  {
+    name: 'Tryndamere',
+    promedio: 137.24,
+  },
+  {
+    name: 'Garen',
+    promedio: 136.03,
+  },
+  {
+    name: 'Trundle',
+    promedio: 135.97,
+  },
+  {
+    name: 'Zac',
+    promedio: 135.07,
+  },
+  {
+    name: 'Alistar',
+    promedio: 134.92,
+  },
+];
+describe('ORDENAR DE FORMA ASCENDENTE Y DESCENDENTE', () => {
   it('orderData es una funcion', () => {
     expect(typeof orderData).toBe('function');
   });
-  it('Debe ordenar de manera ascendente', () => {
-    expect(orderData(champAscend, 'name', 'ascendente')).toEqual(champAscend);
+  it('Debería ordenar de manera ascendente', () => {
+    expect(orderData(champA, 'name', 'ascendente')).toEqual(champA);
   });
-  it('Debe ordenar de manera descendente', () => {
-    expect(orderData(champDescend, 'name', 'descendente')).toEqual(champDescend);
+  it('Debería ordenar de manera descendente', () => {
+    expect(orderData(champD, 'name', 'descendente')).toEqual(champD);
   });
 });
-
-describe('searchChampions', () => {
-  it('searchChampions es una funcion', () => {
+describe('FILTRAR POR ROLES', () => {
+  it('orderFilterTags es una funcion', () => {
+    expect(typeof orderFilterTags).toBe('function');
+  });
+  it('Debería retornar los campeones de Rol: Assessin', () => {
+    expect(orderFilterTags(champA, 'tags', 'Assassin')).toEqual(rolAsse);
+  });
+  it('Debería retornar los campeones de Rol: Tank', () => {
+    expect(orderFilterTags(champA, 'tags', 'Tank')).toEqual(rolTank);
+  });
+  it('Debería retornar los campeones de Rol: Support', () => {
+    expect(orderFilterTags(champA, 'tags', 'Support')).toEqual(rolSupport);
+  });
+});
+describe('BUSCAR POR NOMBRES', () => {
+  it('searchChampions() es una función:', () => {
     expect(typeof searchChampions).toBe('function');
   });
-  it('Deberá de mostrar un array', () => {
-    expect(typeof searchChampions([champAscend])).toBe('object');
+  it('Debería retornar los campeones que contiene en su nombre la letra s', () => {
+    expect(searchChampions(champA, 'name', 's')).toEqual(searchA);
+  });
+});
+describe('CÁLCULO ESTADÍSTICO - PROMEDIO ESTADÍSTICO DE DEFENSA', () => {
+  it('dataOrdenado() es una función:', () => {
+    expect(typeof dataOrdenado).toBe('function');
+  });
+  it('Debería retornar a los campones con mejores puntajes', () => {
+    expect(dataOrdenado(calculo)).toEqual(ordenado);
   });
 });
